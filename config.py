c9be6a999aa460be632111f7fee526c0858d1bcc
XaltriX <129171425+XaltriX@users.noreply.github.com>
2025-10-23 19:40:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,28 +9,28 @@ from logging.handlers import RotatingFileHandler
 #Bot token @Botfather, --⚠️ REQUIRED--
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 #Your API ID from my.telegram.org --⚠️ REQUIRED--
-APP_ID = int(os.environ.get("APP_ID", "24371796"))
+APP_ID = int(os.environ.get("APP_ID", "24955235"))
 
 #Your API Hash from my.telegram.org, --⚠️ REQUIRED--
-API_HASH = os.environ.get("API_HASH", "8121c78f4b8b31e88cc2623d1277338d")
+API_HASH = os.environ.get("API_HASH", "f317b3f7bbe390346d8b46868cff0de8")
 
 #Your db channel Id --⚠️ REQUIRED--
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002667555990"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002004278204"))
 
 #OWNER ID --⚠️ REQUIRED--
-OWNER_ID = int(os.environ.get("OWNER_ID", "7387793694"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "5706788169"))
 
 #SUPPORT_GROUP: This is used for normal users for getting help if they don't understand how to use the bot --⚠ OPTIONAL--
-SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP", "-1002045544935")
+SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP", "-1002309557046")
 
 #Port
-PORT = os.environ.get("PORT", "8050")
+PORT = os.environ.get("PORT", "8080")
 
 #Database --⚠️ REQUIRED--
-DB_URL = os.environ.get("DB_URL", "mongodb+srv://AnimeRavenBots:AnimeRavenBots@animeravenbots.huekk.mongodb.net/?retryWrites=true&w=majority&appName=BlackGokuRavenBot")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://eno2223456:7Cdmqig5Ih2vrqW4@cluster0.ccpmee5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DATABASE_NAME", "HonistaRobot")
 
-VERIFY_DB = os.environ.get("VERIFY_DB", "mongodb+srv://dattebayo56:dattebayo56@animeravenbots.6wcgy.mongodb.net/?retryWrites=true&w=majority")
+VERIFY_DB = os.environ.get("VERIFY_DB", "mongodb+srv://luffydb:12345@cluster0.nue5da5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DBV_NAME = os.environ.get("VERIFY_DBNAME", "Honista")
 
 
@@ -38,7 +38,7 @@ TOKEN_PIC = os.environ.get("TOKEN_PIC", "https://graph.org/file/f03274dfad4979c8
 
 
 #Tutorial video for the user of your shortner on how to download.
-TUT_VID = os.environ.get("TUT_VID","https://t.me/ChipsTutorial/7")
+TUT_VID = os.environ.get("TUT_VID","https://t.me/HTODLINKZ/2")
 
 
 START_PIC = os.environ.get("START_PIC", "https://graph.org/file/8263af1da9ac363d8c580-a8912d879836f10ccd.jpg")
